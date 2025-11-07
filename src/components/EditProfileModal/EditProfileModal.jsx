15715249a2e1d03576ad649efbc3c67b23e246b5
Filip Milosevic <filipmcetinje@gmail.com>
2025-11-07 13:24:04 -0500

    Project 14

--- src/components/EditProfileModal/EditProfileModal.jsx
+++ src/components/EditProfileModal/EditProfileModal.jsx
@@ -1,3 +1,4 @@
+import "./EditProfileModal.css";
 import { useState, useContext, useEffect } from "react";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import CurrentUserContext from "../../contexts/CurrentUserContext";

57a1e4113daf064dcca1239b31c419e55076ccc9
Filip Milosevic <filipmcetinje@gmail.com>
2025-12-18 17:51:03 -0500

    Updated se_project_react

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,9 +1,12 @@
 import { checkResponse } from "./api";
 
-const BASE_URL = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.filip-wtwr.undo.it"
+    : "http://localhost:3001";
 
 export function signup({ name, avatar, email, password }) {
-  return fetch(`${BASE_URL}/signup`, {
+  return fetch(`${baseUrl}/signup`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, avatar, email, password }),
@@ -11,7 +14,7 @@ export function signup({ name, avatar, email, password }) {
 }
 
 export function signin({ email, password }) {
-  return fetch(`${BASE_URL}/signin`, {
+  return fetch(`${baseUrl}/signin`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
@@ -19,7 +22,7 @@ export function signin({ email, password }) {
 }
 
 export function checkToken(token) {
-  return fetch(`${BASE_URL}/users/me`, {
+  return fetch(`${baseUrl}/users/me`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
